a7d1d75255cf5465b9e0db36b1c69e983116f8a0
Zoila Yaneth <yanethvillatoro444@yahoo.com>
2019-05-05 00:23:35 -0600

    Plantilla de permisos

--- src/main/java/com/bad/core/repository/RolRepository.java
+++ src/main/java/com/bad/core/repository/RolRepository.java
@@ -2,11 +2,16 @@ package com.bad.core.repository;
 
 import java.util.List;
 
+
 import org.springframework.stereotype.Repository;
 
 import com.bad.core.entity.Authority;
+import com.bad.core.entity.Privilege;
 
 @Repository
-public interface RolRepository {
+public interface RolRepository{
 	public List<Authority> findAll();
+	public Authority findOne(Long id);	
+	public List<Privilege> findAllPrivilege();
+
 }
