795f1e4c2a95c3236b1744ffca1375e106792cb6
ReynaldoNavarro <reynaldonavarro14@gmail.con>
2019-05-13 13:01:42 -0600

    Probar NUEVO Usuario

--- src/main/java/com/bad/core/repository/UserRepository.java
+++ src/main/java/com/bad/core/repository/UserRepository.java
@@ -5,12 +5,11 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.bad.core.entity.Users;
-import com.bad.core.entity.Authority;
 
 @Repository
 public interface UserRepository extends CrudRepository<Users, Long>{
 	public Optional<Users> findByUsername(String username);
-	public Users findOne(Long id);
+	//public Users findOne(Long id);
 	public List<Users> findAll();
 	
 	//public void save (Users user);
